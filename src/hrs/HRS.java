6e68a9a4b17ba0bffacabd98f1260d966b7d33bf
İsmail Altun <tirigan@windowslive.com>
2018-10-19 21:56:30 +0300

    Update HRS.java

--- src/hrs/HRS.java
+++ src/hrs/HRS.java
@@ -12,10 +12,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author TalhaBadik
- */
+
 public class HRS extends Application {
     
     @Override
